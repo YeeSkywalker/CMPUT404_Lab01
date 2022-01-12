ca7a93a00b4ae69f5db8e56e05e6b51c2bc65989
YeeSkywalker <itsyeelin@gmail.com>
2022-01-11 18:28:24 -0800

    Lab1 v1.1 Add Github Raw url

--- Lab1.py
+++ Lab1.py
@@ -4,3 +4,6 @@ print("Request version: " + requests.__version__)
 
 google = requests.get("http://www.google.com")
 print("Request Google Status: " + str(google.status_code))
+
+git = requests.get("https://raw.githubusercontent.com/YeeSkywalker/CMPUT404_Lab01/main/Lab1.py")
+print("Request Github Python File" + git.text)
